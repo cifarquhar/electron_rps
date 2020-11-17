3afebce2691f4ef594fd0d1c53303958aad4d108
Colin Farquhar <colin.farquhar@codeclan.com>
2020-11-17 14:49:53 +0000

    add spring boot files

---
+++ rps_backend/src/test/java/com/colin/learning/rps_backend/RpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colin.learning.rps_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
