c0c0e5a03d984750213cad0f1a6a01b57547ced0
Colin Farquhar <colin.farquhar@codeclan.com>
2020-11-18 14:43:19 +0000

    add .jar and config main.js to start/stop backend

--- rps_frontend/main.js
+++ rps_frontend/main.js
@@ -1,4 +1,5 @@
 const {app, BrowserWindow} = require('electron');
+const kill = require('kill-port');
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -8,18 +9,30 @@ const createWindow = () => {
       nodeIntegration: true
     }
   });
-
+  
   win.loadFile('index.html');
+  
+  win.on('closed', () => {
+    console.log("closing down");
+    kill(8080, 'tcp')
+      .then(console.log("killed port 8080"));
+  })
 }
 
+const jarPath = app.getAppPath() + '/rps_backend-0.0.1-SNAPSHOT.jar';
+console.log("jarPath:", jarPath);
+const child = require('child_process').spawn(
+  'java', ['-jar', jarPath, '']
+  );
+  
 app.whenReady().then(createWindow);
-
+  
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
+  
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
